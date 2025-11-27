a3eafada169626359d853f7b17cb527a79f23d2a
Sutapa27 <sutapa272002@gmail.com>
2025-11-28 03:56:57 +0530

    bug fixed

--- public/js/script.js
+++ public/js/script.js
@@ -34,22 +34,15 @@ if (searchInput) {
   // When user focuses on input (keyboard opens)
   searchInput.addEventListener('focus', () => {
     searchBar.classList.add('focused');
-    // Disable scroll hiding while typing
-    window.removeEventListener('scroll', handleScroll);
   });
 
   // When user is done (keyboard closes)
   searchInput.addEventListener('blur', () => {
-    setTimeout(() => {
-      searchBar.classList.remove('focused');
-      // Re-enable scroll hiding
-      window.addEventListener('scroll', handleScroll);
-    }, 300);
+    searchBar.classList.remove('focused');
   });
 }
 
-// Move scroll handler to a named function
-function handleScroll() {
+window.addEventListener("scroll", function () {
   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
   if (scrollTop > lastScrollTop) {
@@ -59,7 +52,4 @@ function handleScroll() {
   }
 
   lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
-}
-
-// Use the named function
-window.addEventListener("scroll", handleScroll);
+});
